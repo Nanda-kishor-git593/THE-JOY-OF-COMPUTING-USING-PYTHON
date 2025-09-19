eec73444ed7a475e932ac88a969a19ec701a0a75
Pemmaka Nanda kishor reddy <pemmakanandakishor@gmail.com>
2025-09-19 00:42:55 -0700

    Add files via upload

---
+++ w2 - program1.py
@@ -0,0 +1,30 @@
+
+
+print("hello world");
+
+t1 = "amit"
+print(t1)
+t2 = "simran"
+t3 = "vidya"
+print("hello t1")
+print("hello",t1)
+print("hello",t2)
+print("hello",t3)
+i = "sudarshan"
+print("hello",i)
+print("hello",t1,t2,t3)
+print("hello",t1,t2,t3,",how are you all doing today ?")
+t1 = "Amit_Varma"
+print(t1)
+t1= "India"
+print(t1)
+t1 = "Amit_Varma"
+print("hello",t1,t2,t3,",how are you all doing today ?")
+print("hello",t1,t2,t3,".how are you all doing today ?")
+age=30
+print("The number you entered was ", age)
+age =40
+print("The number you entered was ", age)
+print(age)
+age = age+1
+print(age)
